6d03efa86b6c0270f28ee89a7b4c922235f29077
Assemoka <zhanabaikyzy2017@gmail.com>
2018-01-18 16:51:25 +0600

    asd

---
+++ LAB 2/Lab2/Lab2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void f1()
+        {
+            FileStream fs = new FileStream(@"C:\Users\HP\Documents\asd.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string  s;
+            string[] ss;
+            s = sr.ReadLine();
+            ss = s.Split(' ');
+            int m = int.Parse(ss[0]);
+            int mi = int.Parse(ss[0]);
+            for(int i = 0; i < ss.Length; i++)
+            {
+                if(int.Parse(ss[i]) >= m)
+                {
+                    m = int.Parse(ss[i]);
+                }
+                if (int.Parse(ss[i]) <= mi)
+                {
+                    mi = int.Parse(ss[i]);
+                }
+
+            }
+
+            Console.WriteLine(m + " " + mi);
+
+        }
+        static void Main(string[] args)
+        {
+            f1();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ LAB 2/Lab3/Lab3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static bool IsPrime(int a)
+        {
+            bool asd = true;
+            if(a == 1)
+            {
+                asd = false;
+            }
+            if (a == 2)
+            {
+                asd = true;
+            }
+            for(int i = 2; i >= Math.Sqrt(a); i++)
+            {
+                if (a % i == 0)
+                {
+                    asd = false;
+                    break;
+                }
+            }
+            return asd;
+
+        }
+        static void f1()
+        {
+            FileStream fr = new FileStream(@"C:\Users\HP\Documents\asd.txt",FileMode.Open , FileAccess.Read);
+            StreamReader sr = new StreamReader(fr);
+            string s = sr.ReadLine();
+            string[] ss;
+            ss = s.Split(' ');
+            
+            int[] a = new int [ss.Length];
+            for(int j = 0; j < ss.Length; j++)
+            {
+                if(IsPrime(int.Parse(ss[j])) == true)
+                {
+                   a[j] = int.Parse(ss[j]);
+    
+                }
+            }
+            int m = a[0];
+            for(int i = 0; i <= a.Length; i++)
+            {
+                if(a[i] <= m)
+                {
+                    m = a[i];
+                }
+            }
+            sr.Close();
+            fr.Close();
+
+
+             FileStream fs = new FileStream(@"C:\Users\HP\Documents\asd.txt", FileMode.Open, FileAccess.Write);
+            StreamWriter ds = new StreamWriter(fs);
+            ds.WriteLine(m);
+            ds.Close();
+            fs.Close();
+
+            
+        }
+      
+        static void Main(string[] args)
+        {
+            f1();
+
+
+        }
+    }
+}
